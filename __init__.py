59d80e095031f12554247dd373d27c2638744472
Ekaterina270191 <52824370+Ekaterina270191@users.noreply.github.com>
2022-09-23 16:43:28 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
-from .find_index import find_index_previous_word, most_often_word
+from .find_index import find_index_previous_word
+from .most_often import , most_often_word
 
 __all__= ('find_index_previous_word', 'most_often_word')
