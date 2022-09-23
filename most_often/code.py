1e7f2515aca1f5076b609814ce76cb429243635e
Ekaterina270191 <52824370+Ekaterina270191@users.noreply.github.com>
2022-09-23 16:26:14 +0300

    Create code.py

---
+++ most_often/code.py
@@ -0,0 +1,14 @@
+input_line = "apple orange banana banana orange"                                        
+dict_total = {}
+
+def most_often_word(string=input_line):
+    
+    for simvol in string.split():
+        
+        if simvol not in dict_total:
+            dict_total[simvol] = 1
+        else:
+            dict_total[simvol] += 1
+                
+    return sorted([k for k, v in  dict_total.items() if v == max(dict_total.values())])[0] # Вывод одного ключа с максималльным значением - banana
+                                                                                           # если несколько ключей надо вывести, то убираем [0]
