49f339f32f0a850147c3cb4e4112a7338f911817
Ekaterina270191 <52824370+Ekaterina270191@users.noreply.github.com>
2022-09-23 17:16:14 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from .find_index import find_index_previous_word
 from .most_often import most_often_word
-from .count import count_occurrences
+from .count_occurrences import count_occurrences
 
 __all__= ('find_index_previous_word', 'most_often_word', 'count_occurrences')
