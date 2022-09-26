1908712089354c31b66302e9edef29050e2e9639
Ekaterina270191 <52824370+Ekaterina270191@users.noreply.github.com>
2022-09-26 11:57:45 +0300

    Update code.py

--- count_occurrences/code.py
+++ count_occurrences/code.py
@@ -1,1 +1,14 @@
+input_line = "one two one tho three"
+my_dict = {}
+lst = []
 
+def count_occurrences(string=input_line):
+    
+    for word in string.split():
+        if word not in my_dict:
+            my_dict[word] = 0 
+        else: 
+            my_dict[word] += 1
+            
+        lst.append(my_dict[word])
+    return ' '.join(list(map(str, lst)))
